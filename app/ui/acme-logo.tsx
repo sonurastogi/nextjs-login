5ab66c2e2d4beab6c1aff8664b9401e5778a955f
sonurastogi <sonurastogi361@gmail.com>
2024-04-07 22:55:31 +0530

    check

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,12 +1,12 @@
 //import { GlobeAltIcon } from '@heroicons/react/24/outline';
 //import { lusitana } from './fonts';
 
-export default function AcmeLogo() {
-  return (
-    <>
-
-   <h1> hello </h1>
+//export default function AcmeLogo() {
+//  return (
+//    <>
+//
+//  <h1> hello </h1>
    
-   </>
-  );
-}
+ //  </>
+//  );
+//}
