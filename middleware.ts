73bf059f320991ed8d1ee1b33a643180f468e296
sonurastogi <sonurastogi361@gmail.com>
2024-04-08 08:20:30 +0530

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+
  
 export default NextAuth(authConfig).auth;
  
@@ -7,3 +8,4 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
